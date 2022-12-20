0af194e9a95d4ff90ae5ba212e67135b72f58b41
pushkarguptaaa <pushkardps8@gmail.com>
2022-12-20 19:46:56 +0530

    php output

---
+++ output.php
@@ -0,0 +1,22 @@
+<?php
+//echo 123, 'hello'. 1.5;
+//print 123;
+//print_r([1,2,3,4]);
+//var_dump("Hello");
+//var_dump(true);
+//var_export('hi');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?= 'hi' ?>
+    </h1>
+</body>
+</html>
